efab02ff5a8c53259e33d4660de2842413352504
Walter-Lauxmann <100165427+Walter-Lauxmann@users.noreply.github.com>
2023-05-31 14:23:13 -0300

    Formulario de envío

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CarritoService } from '../carrito.service';
 
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.component.html',
-  styleUrls: ['./carrito.component.css']
+  styleUrls: ['./carrito.component.css'],
 })
-export class CarritoComponent {
+export class CarritoComponent implements OnInit {
+  items = this.carritoService.getItems();
+  total = 0;
 
-  items= this.carritoService.getItems();
+  formularioChequeo = this.formBuilder.group({
+    nombre: '',
+    direccion: '',
+  });
 
-  constructor(private carritoService: CarritoService) {}
+  constructor(
+    private carritoService: CarritoService,
+    private formBuilder: FormBuilder
+  ) {}
 
+  ngOnInit() {
+    for (let item of this.items) {
+      this.total += item.precio;
+    }
+  }
 
-
+  onSubmit(): void {
+    // Procesamos los datos a chequear
+    this.items = this.carritoService.limpiarCarrito();
+    console.warn('Su orden ha sido enviada', this.formularioChequeo.value);
+    this.formularioChequeo.reset();
+  }
 }
